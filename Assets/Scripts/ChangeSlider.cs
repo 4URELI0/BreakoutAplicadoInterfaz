be85b011d4094eb324aa2df73577df7fd5b62fa5
Neru <ntneru.94@gmail.com>
2025-11-16 03:40:11 -0300

    Cambiando Sprites

--- Assets/Scripts/ChangeSlider.cs
+++ Assets/Scripts/ChangeSlider.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class ChangeSlider : MonoBehaviour
 {
     private Slider slider;
+    [SerializeField] private AudioMixer audioMixer;
 
     void Awake()
     {
         slider = GetComponent<Slider>();
-        slider.value = 0;
+        float currentVolume;
+        audioMixer.GetFloat("Volumen", out currentVolume);
+        Debug.Log("Volumen Actual: del mixer " + currentVolume);
+        slider.value = currentVolume;
     }
 
     public void AddValue(float value)
